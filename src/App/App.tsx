9c1071c927737a233d1768d4a6183fd8554d374e
kwabendarkwa <darkwak@live.com>
2023-05-05 01:34:23 +0000

    :lipstick: create app folder in src and button folder also

---
+++ src/App/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';import {styles} from './App.stylesheet'
+//used to serve the first screen of the application 
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
+
