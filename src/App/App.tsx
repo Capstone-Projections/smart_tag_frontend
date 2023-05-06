f25133a568aea4ab84ed9ca39b8b0390aa8167b6
kwabendarkwa <darkwak@live.com>
2023-05-06 21:08:01 +0000

    :recycle: text component done with splash screen error

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React , { useState } from 'react';
 import {Text,View} from 'react-native';
 import {styles} from './App.stylesheet';
 import { AppText } from '../components/general/Text';
-
+import  {loadFonts}  from './font'
+import Apploading from "expo-app-loading";
 
 
 export default function App() {
+  const [fontsloaded, setFontsLoaded] = useState(false);
+  if (fontsloaded) {
   return (
 
     <View  style = {styles.container}>
       <Text >
     Hello Heading 1
     </Text>
-    <AppText.Subtitle>Hey What's up</AppText.Subtitle>
+    <AppText>Hey What's up</AppText>
     </View>
     
-  );
+  );}
+  else {
+    return (
+      <Apploading
+        startAsync={loadFonts}
+        onFinish={() => {
+          setFontsLoaded(true);
+        }}
+        onError={console.warn}
+      />
+    );
+  }
 }
 
