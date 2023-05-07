456890b03d6cd30803084436c8ddf5a610c75540
kwabendarkwa <darkwak@live.com>
2023-05-07 17:22:56 +0000

    :bug: fix splash screen error

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import React , { useState } from 'react';
+import React , { useEffect} from 'react';
 import {Text,View} from 'react-native';
 import {styles} from './App.stylesheet';
 import { AppText } from '../components/general/Text';
@@ -6,13 +6,37 @@ import {useFonts} from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
 
+
+
 export default function App() {
+
+  const [fontsloaded] = useFonts ({
+    'Poppins': require('../../assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Bold': require('../../assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-ExtraBold': require('../../assets/fonts/Poppins-ExtraBold.ttf'),
+    'Poppins-ExtraLight': require('../../assets/fonts/Poppins-ExtraLight.ttf'),
+    'Poppins-Light': require('../../assets/fonts/Poppins-Light.ttf'),
+    'Poppins-Medium': require('../../assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-SemiBold': require('../../assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Thin': require('../../assets/fonts/Poppins-Thin.ttf'),
+  })
+
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  },[]);
+
+  if(!fontsloaded){
+    return null;
+  }else{
+    SplashScreen.hideAsync();
+  }
 
   return (
     <View  style = {styles.container}>
-      <Text >
-    Hello Heading 1
-    </Text>
     <AppText>Hey What's up</AppText>
     </View>
     
