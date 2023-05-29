553c0cf8c0bb77a918fcadfe6726bc13209d865a
kwabendarkwa <darkwak@live.com>
2023-05-29 11:41:07 +0000

    :lipstick: Fixes problem with folder naming

--- src/components/general/Text/AppText.tsx
+++ src/components/general/Text/AppText.tsx
@@ -49,4 +49,5 @@ export const Text = styled.Text<AppTextProps>`
       : props.type === 'ustudent'
       ? 'center'
       : 'left'};
+      
 `;
--- src/components/general/Text/index.ts
+++ src/components/general/Text/index.ts
@@ -1,1 +1,2 @@
+
 export { Text } from './AppText';
