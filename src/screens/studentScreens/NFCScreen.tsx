609d87b89e6eafa93d23000bbaed9630a4696390
Blay <blay78792@gmail.com>
2023-06-07 13:57:28 +0000

    Finished Styling the NFC Image

--- src/screens/studentScreens/NFCScreen.tsx
+++ src/screens/studentScreens/NFCScreen.tsx
@@ -1,13 +1,22 @@
-import { View, Text,Image } from 'react-native'
+import { View, Text,Image,StyleSheet } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const NFCScreen = () => {
   return (
-    <SafeAreaView >
+    <SafeAreaView style={styles.container}>
      <Image source={require('../../../assets/images/chip.png')}  style={{width: 400, height: 400}} />
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    
+  });
 
 export default NFCScreen;
