16fed8e29c66f750c57c9509ff5beb03afb491b8
kwabendarkwa <darkwak@live.com>
2023-07-02 12:19:52 +0000

    :sparkles: Add course context provider
    this commit adds a coourse context provider to the course card so that the id of a particular course can be accessed by children of that course card

--- src/components/general/CoursesCard/CourseCard.tsx
+++ src/components/general/CoursesCard/CourseCard.tsx
@@ -1,12 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { View, ScrollView, RefreshControl } from 'react-native';
+import { View } from 'react-native';
 import { Card, Title, Paragraph } from 'react-native-paper';
 import { AuthContext } from '../../../context/AuthContext';
-import axios from 'axios';
 import { styles } from './styles';
 import { CardProps } from './props';
 import { getRandomColor } from '../../../services/getRandomColor';
+import { CourseProvider } from '../../../context/CourseContext';
 
 export const CourseCard: React.FC<CardProps> = ({ name, courseCode }) => {
     const [cardColor, setCardColor] = useState(getRandomColor());
@@ -25,16 +25,20 @@ export const CourseCard: React.FC<CardProps> = ({ name, courseCode }) => {
     };
 
     return (
-        <View style={styles.cardContainer}>
-            <Card
-                style={[styles.card, { backgroundColor: cardColor }]}
-                onPress={handleCardPress}
-            >
-                <Card.Content>
-                    <Title style={styles.title}>{name}</Title>
-                    <Paragraph style={styles.paragraph}>{courseCode}</Paragraph>
-                </Card.Content>
-            </Card>
-        </View>
+        <CourseProvider>
+            <View style={styles.cardContainer}>
+                <Card
+                    style={[styles.card, { backgroundColor: cardColor }]}
+                    onPress={handleCardPress}
+                >
+                    <Card.Content>
+                        <Title style={styles.title}>{name}</Title>
+                        <Paragraph style={styles.paragraph}>
+                            {courseCode}
+                        </Paragraph>
+                    </Card.Content>
+                </Card>
+            </View>
+        </CourseProvider>
     );
 };
