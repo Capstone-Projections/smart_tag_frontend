a5952a8cdbf6b10801821c60ea472a3d57f6cab7
kwabendarkwa <darkwak@live.com>
2023-07-12 11:19:20 +0000

    :sparkles: Add first version of nfc feature

--- src/screens/studentScreens/NFCScreen/NFCScreen.tsx
+++ src/screens/studentScreens/NFCScreen/NFCScreen.tsx
@@ -1,16 +1,31 @@
-import { View, Text, Image, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useCallback } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from './styles';
+import useNfcTagReader from '../../../hooks/NFC/useNFCTagReader';
 
 const NFCScreen = () => {
+    const tagData = useNfcTagReader();
+
+    const handlePress = useCallback(() => {
+        if (!tagData) {
+            useNfcTagReader();
+        }
+    }, [tagData]);
+
     return (
-        <SafeAreaView style={styles.container}>
-            <Image
-                source={require('../../../assets/images/chip.png')}
-                style={{ width: 400, height: 400 }}
-            />
-        </SafeAreaView>
+        <View style={styles.wrapper}>
+            <TouchableOpacity onPress={handlePress}>
+                <Text>{tagData ? 'Scanning NFC Tag...' : 'Scan a Tag'}</Text>
+            </TouchableOpacity>
+
+            {tagData && (
+                <View style={styles.tagContainer}>
+                    <Text style={styles.tagText}>Tag found:</Text>
+                    <Text style={styles.tagText}>{tagData}</Text>
+                </View>
+            )}
+        </View>
     );
 };
 
