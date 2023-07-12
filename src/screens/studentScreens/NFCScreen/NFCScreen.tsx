fc54fe63d63d3edb60d9afdf349e5a9723a5ab85
kwabendarkwa <darkwak@live.com>
2023-07-12 13:01:48 +0000

    :recycle: Refactor nfc code to working version

--- src/screens/studentScreens/NFCScreen/NFCScreen.tsx
+++ src/screens/studentScreens/NFCScreen/NFCScreen.tsx
@@ -1,32 +1,67 @@
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import React, { useCallback } from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { styles } from './styles';
-import useNfcTagReader from '../../../hooks/NFC/useNFCTagReader';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import NfcManager, { NfcTech } from 'react-native-nfc-manager';
 
-const NFCScreen = () => {
-    const tagData = useNfcTagReader();
+// Pre-step, call this before any NFC operations
+NfcManager.start();
 
-    const handlePress = useCallback(() => {
-        if (!tagData) {
-            useNfcTagReader();
+function NFCScreen() {
+    const [tagData, setTagData] = useState('');
+
+    async function readNdef() {
+        try {
+            // register for the NFC tag with NDEF in it
+            await NfcManager.requestTechnology(NfcTech.Ndef);
+            // the resolved tag object will contain `ndefMessage` property
+            const tag = await NfcManager.getTag();
+
+            if (tag) {
+                const payload = tag.ndefMessage[0].payload;
+                const text = String.fromCharCode.apply(null, payload.slice(3));
+
+                setTagData(text);
+            }
+        } catch (ex) {
+            console.warn('Oops!', ex);
+        } finally {
+            // stop the nfc scanning
+            NfcManager.cancelTechnologyRequest();
         }
-    }, [tagData]);
+    }
 
     return (
         <View style={styles.wrapper}>
-            <TouchableOpacity onPress={handlePress}>
-                <Text>{tagData ? 'Scanning NFC Tag...' : 'Scan a Tag'}</Text>
+            <TouchableOpacity onPress={readNdef}>
+                <Text>Scan a Tag</Text>
             </TouchableOpacity>
 
             {tagData && (
                 <View style={styles.tagContainer}>
                     <Text style={styles.tagText}>Tag found:</Text>
-                    <Text style={styles.tagText}>{tagData}</Text>
+                    <Text style={styles.tagText}>
+                        {JSON.stringify(tagData)}
+                    </Text>
                 </View>
             )}
         </View>
     );
-};
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    tagContainer: {
+        marginTop: 20,
+        alignItems: 'center',
+    },
+    tagText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+});
 
 export default NFCScreen;
